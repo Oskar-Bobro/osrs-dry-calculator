bf24a7f14ec50621661f6db544f2703fa16e3227
Oskar Bobro <46914172+Oskar-Bobro@users.noreply.github.com>
2026-09-23 21:38:33 +0000

    Calculate chance of still being dry after n kills

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function updateResult() {
-  const killsPerhour = killsInputElement.valueAsNumber;
+  const killsPerHour = killsInputElement.valueAsNumber;
   const dropRate = dropRateElement.valueAsNumber;
-  const expectedHours = dropRate / killsPerhour;
+  const expectedHours = dropRate / killsPerHour;
+  const killsSoFar = killsSoFarElement.valueAsNumber;
+  const rate = 1 / dropRate;
+  const dryChance = (1 - rate) ** killsSoFar;
   resultElement.textContent = `Expected kills: ${dropRate} kills`;
   resultElement.textContent += `, Expected time: ${expectedHours.toFixed(1)} hours`;
+  resultElement.textContent += `, % of being dry: ${(dryChance * 100).toFixed(1)}%`;
 }
 
 const resultElement = document.getElementById("result");
 const killsInputElement = document.getElementById("killsInput");
 const dropRateElement = document.getElementById("dropRateInput");
+const killsSoFarElement = document.getElementById("killsSoFarInput");
 
 killsInputElement.addEventListener("input", updateResult);
 dropRateElement.addEventListener("input", updateResult);
+killsSoFarElement.addEventListener("input", updateResult);
